45ea013f57a0c1219b19cebaf9c5a84cb22d700e
Kartik Kochhar <kochharkartik51@gmail.com>
2023-07-31 17:12:00 +0530

    initial commit for lesson-1, made initial divs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=()=>{
+return(
+  <div className='categories-container'>
+  <div className='category-container'>
+    {/* <img /> */}
+    <div className='category-body-container'>
+      <h1>Hats</h1>
+      <p>Shop Now</p>
     </div>
-  );
+  </div>
+
+  <div className='category-container'>
+    {/* <img /> */}
+    <div className='category-body-container'>
+      <h1>Jacket</h1>
+      <p>Shop Now</p>
+    </div>
+  </div>
+
+  <div className='category-container'>
+    {/* <img /> */}
+    <div className='category-body-container'>
+      <h1>Sneakers</h1>
+      <p>Shop Now</p>
+    </div>
+  </div>
+
+  <div className='category-container'>
+    {/* <img /> */}
+    <div className='category-body-container'>
+      <h1>Womens</h1>
+      <p>Shop Now</p>
+    </div>
+  </div>
+
+  <div className='category-container'>
+    {/* <img /> */}
+    <div className='category-body-container'>
+      <h1>Mens</h1>
+      <p>Shop Now</p>
+    </div>
+  </div>
+</div>
+
+
+)
+
 }
+
 
 export default App;
