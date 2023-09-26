037088d1786613b9162cddaf5c38ed89de3d8224
Kartik Kochhar <kochharkartik51@gmail.com>
2023-09-26 08:31:23 +0530

    displayed the imported data from firestore to product cards under shop component

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,20 +1,21 @@
 import { createContext , useState, useEffect} from "react";
 import { getCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
 
-export const ProductContext = createContext({
-    products: []
+export const CategoriesContext = createContext({
+    categoriesMap: {},
 })
 
-export const ProductProvider = ({children})=>{
-    const [products, setProducts]=useState([]);
+export const CategoriesProvider = ({children})=>{
+    const [categoriesMap, setCategoriesMap]=useState({});
     useEffect(()=>{
         const getCategoriesMap = async()=>{
             const categoryMap   = await getCollectionAndDocuments()
             console.log(categoryMap)
+            setCategoriesMap(categoryMap)
         }
         getCategoriesMap();
     },[])
-        const value = {products};
-    return <ProductContext.Provider value={value}>{children}</ProductContext.Provider>
+        const value = {categoriesMap};
+    return <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>
 }
 //chang
