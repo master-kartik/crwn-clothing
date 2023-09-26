eb19e1d85833f5090ac34d463c65fc67c017a0d5
Kartik Kochhar <kochharkartik51@gmail.com>
2023-09-26 23:14:55 +0530

    message

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,22 +1,15 @@
 import './shop.styles.scss'
-import { CategoriesContext } from '../../contexts/categories.context'
-import { useContext, Fragment } from "react"
-import CategoryPreview from '../../components/category-preview/category-preview.component'
-
+import { Route, Routes } from 'react-router-dom';
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
 const Shop = ()=>{
-    const {categoriesMap} = useContext(CategoriesContext);
     
     return(
         
-            <div className="shop-container">
-                {
-                    Object.keys(categoriesMap).map((title)=>{
-                        const products = categoriesMap[title]
-                        return <CategoryPreview key={title} title={title} products={products}/>
-                    })
-                }
-
-            </div>
+          <Routes>
+            <Route index element={<CategoriesPreview />}></Route>
+            <Route path=":category" element={<Category />}/>
+          </Routes>
             
 
             
