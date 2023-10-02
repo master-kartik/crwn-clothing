9f61424f78c153297e7510547eecebb6b53c8d8a
Kartik Kochhar <kochharkartik51@gmail.com>
2023-10-02 12:53:37 +0530

    final commit for the v 1

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,10 +1,12 @@
 import "./category-item.styles.scss"
-
+import { Link } from "react-router-dom";
 const CategoryItem = ({ category }) => {
 
   const { imageUrl, title } = category;
   return (
-    <div className='category-container'>
+    
+      <Link to={`/shop/${title.toLowerCase()}`} className='category-container'>
+        
       <div className='background-image' style={{
         backgroundImage: `url(${imageUrl})`
       }} />
@@ -12,7 +14,9 @@ const CategoryItem = ({ category }) => {
         <h2>{title}</h2>
         <p>Shop Now</p>
       </div>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      `
-    </div>
+      </Link>
+    
+    
   )
 }
 export default CategoryItem;
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -14,7 +14,7 @@ const addProductToCart = () => addItemToCart(product)
       <img src={imageUrl} alt={`${name}`} />
       <div className='footer'>
         <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
+        <span className='price'>${price}</span>
       </div>
       <Button buttontype='inverted' onClick={addProductToCart}>Add to cart</Button>
     </div>
